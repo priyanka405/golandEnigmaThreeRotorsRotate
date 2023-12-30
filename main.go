16d8ce7130efd238872562c64b97d38924d87281
priyanka405 <65641311+priyanka405@users.noreply.github.com>
2023-12-30 13:13:53 +0100

    Update main.go

--- main.go
+++ main.go
@@ -10,38 +10,51 @@ type rotor struct {
 	position int
 }
 
+type plugboard map[rune]rune
+
 var reflectorB = "YRUHQSLDPXNGOKMIEBFZCWVJAT"
 var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func main() {
+	rotorI := &rotor{wiring: "EKMFLGDQVZNTOWYHXUSPAIBRCJ"}
+	rotorII := &rotor{wiring: "AJDKSIRUXBLHWTMCQGZNPYFVOE"}
+	rotorIII := &rotor{wiring: "BDFHJLCPRTXVZNYEIWGAKMUSQO"}
 
-	rotorI := rotor{wiring: "EKMFLGDQVZNTOWYHXUSPAIBRCJ"}
-	rotorII := rotor{wiring: "AJDKSIRUXBLHWTMCQGZNPYFVOE"}
-	rotorIII := rotor{wiring: "BDFHJLCPRTXVZNYEIWGAKMUSQO"}
+	plugboardMapping := map[rune]rune{
+		'A': 'Y', 'B': 'R', 'C': 'U', 'D': 'H', 'E': 'Q',
+		'F': 'S', 'G': 'L', 'I': 'P', 'J': 'X', 'K': 'N',
+		'M': 'O', 'T': 'V', 'W': 'Z',
+	}
+
+	plugboard := createPlugboard(plugboardMapping)
+
 	rotorI.position = 0
 	rotorII.position = 0
 	rotorIII.position = 0
 
 	plaintext := "HELLO"
-	encryptedText := enigmaEncrypt(plaintext, rotorI, rotorII, rotorIII)
+	encryptedText := enigmaEncrypt(plaintext, plugboard, rotorI, rotorII, rotorIII)
 	fmt.Println("Plaintext: ", plaintext)
 	fmt.Println("Encrypted Text: ", encryptedText)
 
 	rotorI.position = 0
 	rotorII.position = 0
 	rotorIII.position = 0
-	decryptedText := enigmaDecrypt(encryptedText, rotorI, rotorII, rotorIII)
+	decryptedText := enigmaDecrypt(encryptedText, plugboard, rotorI, rotorII, rotorIII)
 	fmt.Println("Decrypted Text: ", decryptedText)
 }
 
-func enigmaEncrypt(plaintext string, rotors ...rotor) string {
+func enigmaEncrypt(plaintext string, pb plugboard, rotors ...*rotor) string {
 	plaintext = strings.ToUpper(plaintext)
 	var encrypted strings.Builder
 
 	for _, char := range plaintext {
 		if char >= 'A' && char <= 'Z' {
 			// Rotate rotors before encryption
-			rotateRotors(&rotors)
+			rotateRotors(rotors)
+
+			// Apply plugboard
+			char = pb[char]
 
 			// Pass the character through the rotors from right to left
 			char = substitute(char, rotors[2])
@@ -56,6 +69,9 @@ func enigmaEncrypt(plaintext string, rotors ...rotor) string {
 			char = substitute(char, rotors[1])
 			char = substitute(char, rotors[2])
 
+			// Apply plugboard again
+			char = pb[char]
+
 			encrypted.WriteRune(char)
 		} else {
 			// Non-alphabetic characters are not modified
@@ -66,14 +82,17 @@ func enigmaEncrypt(plaintext string, rotors ...rotor) string {
 	return encrypted.String()
 }
 
-func enigmaDecrypt(plaintext string, rotors ...rotor) string {
+func enigmaDecrypt(plaintext string, pb plugboard, rotors ...*rotor) string {
 	plaintext = strings.ToUpper(plaintext)
-	var encrypted strings.Builder
+	var decrypted strings.Builder
 
 	for _, char := range plaintext {
 		if char >= 'A' && char <= 'Z' {
 			// Rotate rotors before encryption
-			rotateRotors(&rotors)
+			rotateRotors(rotors)
+
+			// Apply plugboard
+			char = pb[char]
 
 			char = decrypt(char, rotors[2])
 			char = decrypt(char, rotors[1])
@@ -83,36 +102,48 @@ func enigmaDecrypt(plaintext string, rotors ...rotor) string {
 			char = decrypt(char, rotors[1])
 			char = decrypt(char, rotors[2])
 
-			encrypted.WriteRune(char)
+			// Apply plugboard again
+			char = pb[char]
+
+			decrypted.WriteRune(char)
 		} else {
 			// Non-alphabetic characters are not modified
-			encrypted.WriteRune(char)
+			decrypted.WriteRune(char)
 		}
 	}
 
-	return encrypted.String()
+	return decrypted.String()
 }
 
-func rotateRotors(rotors *[]rotor) {
-	for i := 0; i < len(*rotors); i++ {
-		(*rotors)[i].position++
-		if (*rotors)[i].position >= 26 {
-			(*rotors)[i].position = 0
+func rotateRotors(rotors []*rotor) {
+	for i := 0; i < len(rotors); i++ {
+		rotors[i].position++
+		if rotors[i].position >= 26 {
+			rotors[i].position = 0
 		}
 	}
 }
 
-func substitute(char rune, rotor rotor) rune {
+func substitute(char rune, rotor *rotor) rune {
 	index := (int(char-'A') + rotor.position) % 26
 	return rune(rotor.wiring[index])
 }
 
-func decrypt(char rune, rotor rotor) rune {
-	index := strings.IndexRune(rotor.wiring, char)
+func decrypt(char rune, rotor *rotor) rune {
+	index := (strings.IndexRune(rotor.wiring, char) - rotor.position + 26) % 26
 	return rune(alphabet[index])
 }
 
 func reflector(char rune) rune {
 	index := strings.IndexRune(reflectorB, char)
 	return rune(alphabet[index])
+}
+
+func createPlugboard(mapping map[rune]rune) plugboard {
+	pb := make(plugboard)
+	for k, v := range mapping {
+		pb[k] = v
+		pb[v] = k
+	}
+	return pb
 }
